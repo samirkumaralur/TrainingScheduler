b8cb28a306f3070b65aa7cebef2af27b756c57d8
samir <samiralurhp@gmail.com>
2020-09-01 20:02:27 +0530

    added files

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req,res){
+	res.render('index.hbs');
+});
+
+app.listen(3000, function(){
+	console.log("Started..");
+})
