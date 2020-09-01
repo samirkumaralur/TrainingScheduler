c00cc1014a4990eab4e1f38f7b7302fb1047aaae
samir <samiralurhp@gmail.com>
2020-09-01 21:15:30 +0530

    added the template html files

--- app.js
+++ app.js
@@ -1,20 +1,80 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req,res){
-	res.render('index.hbs');
+app.get("/", function (req, res) {
+  res.render("index.hbs");
 });
 
-app.listen(3000, function(){
-	console.log("Started..");
-})
+app.get("/index", function (req, res) {
+  res.render("index.hbs");
+});
+
+app.get("/buttons", function (req, res) {
+  res.render("buttons.hbs");
+});
+
+app.get("/cards", function (req, res) {
+  res.render("cards.hbs");
+});
+
+app.get("/utilities-color", function (req, res) {
+  res.render("utilities-color.hbs");
+});
+
+app.get("/utilities-color", function (req, res) {
+  res.render("utilities-color.hbs");
+});
+
+app.get("/utilities-border", function (req, res) {
+  res.render("utilities-border.hbs");
+});
+
+app.get("/utilities-animation", function (req, res) {
+  res.render("utilities-animation.hbs");
+});
+
+app.get("/utilities-other", function (req, res) {
+  res.render("utilities-other.hbs");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login.hbs");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register.hbs");
+});
+
+app.get("/forgot-password", function (req, res) {
+  res.render("forgot-password.hbs");
+});
+
+app.get("/404", function (req, res) {
+  res.render("404.hbs");
+});
+
+app.get("/blank", function (req, res) {
+  res.render("blank.hbs");
+});
+
+app.get("/charts", function (req, res) {
+  res.render("charts.hbs");
+});
+
+app.get("/tables", function (req, res) {
+  res.render("tables.hbs");
+});
+
+app.listen(3000, function () {
+  console.log("Started..");
+});
